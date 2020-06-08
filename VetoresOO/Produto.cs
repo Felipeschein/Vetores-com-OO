5088a5e7b398ea2b5e46fbea07cb7c2ae88cdf3f
Felipe Schein <felipe_str@hotmail.com>
2020-06-08 14:30:10 -0300

    projeto vetores com OO

---
+++ VetoresOO/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetoresOO
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto()
+        {
+
+        }
+
+
+    }
+}
